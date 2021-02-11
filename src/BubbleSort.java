3ce4f7406c21f13ad8ead4a8034b457c8da2783e
NIKHIL <NIKHIL@DESKTOP-AF2ERTL>
2021-02-11 22:50:46 +0530

    Bubble Sort

---
+++ src/BubbleSort.java
@@ -0,0 +1,34 @@
+public class BubbleSort {
+
+	
+	public static int[] bubbleSortArray(int arr[])
+    {
+        int n = arr.length;
+        for (int i = 0; i < n-1; i++)
+            for (int j = 0; j < n-i-1; j++)
+                if (arr[j] > arr[j+1])
+                {
+                    int temp = arr[j];
+                    arr[j] = arr[j+1];
+                    arr[j+1] = temp;
+                }
+        return arr;
+    }
+	
+	public static void printArray(int arr[]) {
+		
+		for(int i = 0 ; i < arr.length ; i++) {
+			System.out.print(arr[i] + " ");
+		}
+		System.out.println();
+	}
+	
+	public static void main (String[] args) {
+		
+		int arr[] = {9,8,7,6,5,4,3,2,1};
+		arr = bubbleSortArray(arr);
+		printArray(arr);
+
+	}
+
+}
